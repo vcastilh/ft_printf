2219f4f5d227a57282f6160ae78d117b1587162e
vcastilh <viniciaofelix@gmail.com>
2021-10-20 10:23:42 -0300

    feat: Get the flag from format string
    Allows to get the different types of conversions

--- ft_printf.c
+++ ft_printf.c
@@ -6,11 +6,32 @@
 /*   By: vcastilh <vcastilh@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/20 09:19:11 by vcastilh          #+#    #+#             */
-/*   Updated: 2021/10/20 09:44:45 by vcastilh         ###   ########.fr       */
+/*   Updated: 2021/10/20 10:22:24 by vcastilh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #include "ft_printf.h"
+
+int	get_flag(const char *fmt, va_list ap)
+{
+	int	printed_len;
+
+	printed_len = 0;
+	while (*fmt)
+	{
+		if (*fmt == '%')
+		{	
+			printed_len += indentify_flag(++fmt, ap);
+		}
+		else
+		{
+			printed_len += write(1, fmt, 1);
+			fmt++;
+		}
+	}
+	return (printed_len);
+}
+
 
 int	ft_printf(const char *format, ...)
 {
