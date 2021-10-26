2160703ee84299cf20ddf69f4cc7c7e157f44f22
vcastilh <viniciaofelix@gmail.com>
2021-10-26 10:48:11 -0300

    feat: included stdlib header/functions prototypes

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: vcastilh <vcastilh@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/20 09:20:11 by vcastilh          #+#    #+#             */
-/*   Updated: 2021/10/20 15:35:28 by vcastilh         ###   ########.fr       */
+/*   Updated: 2021/10/26 10:48:03 by vcastilh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -14,7 +14,10 @@
 # define FT_PRINTF_H
 # include <stdarg.h>
 # include <unistd.h>
-int	ft_printf(const char *format, ...);
-int	type_c(int c);
-int	type_s(char *str);
+# include <stdlib.h>
+int		ft_printf(const char *format, ...);
+int		type_c(int c);
+int		type_s(char *str);
+int		type_p(unsigned long ptr);
+size_t	ft_strlen(const char *str);
 #endif
