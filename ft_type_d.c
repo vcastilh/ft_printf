787aa104144449ee5f4121b18b44f7ddbf6c8296
vcastilh <viniciaofelix@gmail.com>
2021-10-27 17:32:35 -0300

    feat: Added function that handles %d and %i
    Added functions that will print a decimal and integer
    Added function prototype to ft_printf.h

--- ft_type_d.c
+++ ft_type_d.c
@@ -1,26 +1,27 @@
 /* ************************************************************************** */
 /*                                                                            */
 /*                                                        :::      ::::::::   */
-/*   ft_printf.h                                        :+:      :+:    :+:   */
+/*   ft_type_d.c                                        :+:      :+:    :+:   */
 /*                                                    +:+ +:+         +:+     */
 /*   By: vcastilh <vcastilh@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
-/*   Created: 2021/10/20 09:20:11 by vcastilh          #+#    #+#             */
-/*   Updated: 2021/10/27 15:47:50 by vcastilh         ###   ########.fr       */
+/*   Created: 2021/10/27 15:19:04 by vcastilh          #+#    #+#             */
+/*   Updated: 2021/10/27 16:49:32 by vcastilh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
-#ifndef FT_PRINTF_H
-# define FT_PRINTF_H
-# include <stdarg.h>
-# include <unistd.h>
-# include <stdlib.h>
-int		ft_printf(const char *format, ...);
-int		type_c(int c);
-int		type_s(char *str);
-int		type_p(unsigned long ptr);
-int		type_d(int n);
-size_t	ft_strlen(const char *str);
-char	*ft_strchr(const char *str, int c);
-char	*ft_itoa(int n);
-#endif
+#include "ft_printf.h"
+
+int	type_d(int	numb)
+{
+	char	*str;
+	int		printed_len;
+
+	if (numb == 0)
+		return (write(1, "0", 1));
+	str = ft_itoa(numb);
+	printed_len = 0;
+	printed_len += write(1, str, ft_strlen(str) * sizeof(char));
+	free(str);
+	return (printed_len);
+}
