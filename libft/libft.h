be9327b5b0424da6f52a965456425ed969ea1c24
vcastilh <viniciaofelix@gmail.com>
2021-10-28 11:51:26 -0300

    feat: Added ft_uitoa() source file to Makefile
    Added its prototype to header file
    Deleted libft static library

--- libft/libft.h
+++ libft/libft.h
@@ -6,7 +6,7 @@
 /*   By: vcastilh <vcastilh@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/08/23 08:47:36 by vcastilh          #+#    #+#             */
-/*   Updated: 2021/09/14 08:02:13 by vcastilh         ###   ########.fr       */
+/*   Updated: 2021/10/28 09:01:31 by vcastilh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -48,4 +48,5 @@ void	ft_putendl_fd(char *s, int fd);
 void	ft_putnbr_fd(int n, int fd);
 char	*ft_strmapi(char const *s, char (*f)(unsigned int, char));
 char	**ft_split(char const *s, char c);
+char	*ft_uitoa(unsigned int n);
 #endif
