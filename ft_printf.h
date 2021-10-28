1f122a39e166e032c985c800ae8114a543903d55
vcastilh <viniciaofelix@gmail.com>
2021-10-28 11:59:58 -0300

    refactor:Removed unnecessary code from ft_printf.h
    Included libft.h on ft_printf.h to remove duplicate prototype
    Added type_u() prototype

--- ft_printf.h
+++ ft_printf.h
@@ -6,7 +6,7 @@
 /*   By: vcastilh <vcastilh@student.42sp.org.br>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/10/20 09:20:11 by vcastilh          #+#    #+#             */
-/*   Updated: 2021/10/27 15:47:50 by vcastilh         ###   ########.fr       */
+/*   Updated: 2021/10/28 11:44:24 by vcastilh         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,12 +15,11 @@
 # include <stdarg.h>
 # include <unistd.h>
 # include <stdlib.h>
+# include "./libft/libft.h"
 int		ft_printf(const char *format, ...);
 int		type_c(int c);
 int		type_s(char *str);
 int		type_p(unsigned long ptr);
 int		type_d(int n);
-size_t	ft_strlen(const char *str);
-char	*ft_strchr(const char *str, int c);
-char	*ft_itoa(int n);
+int		type_u(unsigned int n);
 #endif
